4657aceda0f8fec34259d92bc0394d009db34c33
Shivam <shivamjgec@gmail.com>
2024-07-23 11:06:19 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Navigation from './components/Navigation'
+import Hero from './components/Hero'
+import Add from './components/Add'
+
+const App = () => {
+  return <div>
+    <Navigation/> 
+    <Hero/>
+    <Add/>
+    
+  </div>
+}
+
+export default App
---
+++ src/components/Add.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <div>
+    <div className="shopping">
+        <p>Also Available On</p>
+    </div>
+    <div className="brand-icon">
+        <img src="/images/amazon.png" alt="amazon-logo" />
+        <img src="/images/flipkart.png" alt="flipkart-logo" />
+    </div>
+    </div>
+  )
+}
+
+export default Add
